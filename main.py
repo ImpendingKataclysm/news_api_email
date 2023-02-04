848bdd64cd20c91a5c46e52d489600e425d84452
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-04 10:51:45 -0800

    Send email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import requests
+from send_email import send_email
 
 api_key = "71cb0bc3fbcd4f0db7204030105f3090"
-url = "https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&" \
-      "apiKey=71cb0bc3fbcd4f0db7204030105f3090"
+url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&" \
+      f"apiKey={api_key}"
 
 request = requests.get(url)
 content = request.json()
 
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["content"] + 2 * "\n"
+
+body = body.encode('utf-8')
+
+send_email(body)
