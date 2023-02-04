06e84b6de287830554b4411493c1cc7fbf2eeaac
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-04 11:07:30 -0800

    Multiple amendments: links, language restriction, add subject to email, control news topic

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import requests
 from send_email import send_email
 
+topic = "python"
 api_key = "71cb0bc3fbcd4f0db7204030105f3090"
-url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&" \
-      f"apiKey={api_key}"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}" \
+      "&sortBy=publishedAt&" \
+      f"apiKey={api_key}&" \
+      "language=en"
 
 request = requests.get(url)
 content = request.json()
 
-body = ""
+body = "Subject: Today's News\n"
 
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["content"] + 2 * "\n"
+        body = body \
+               + article["title"] + "\n" \
+               + article["description"] + "\n" \
+               + article["url"] \
+               + 2 * "\n"
 
 body = body.encode('utf-8')
 
